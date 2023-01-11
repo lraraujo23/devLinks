bc327c3fa349b118072bce12ea8920de5f494c22
lraraujo23 <lraraujo23@gmail.com>
2023-01-11 18:42:08 -0300

    Projeto DevLink Feito junto com a rocketseat

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement    
+    //if(html.classList.contains('light')){
+    //    html.classList.remove('light')
+    //}
+    //else {
+    //    html.classList.add('light')
+    //}
+    html.classList.toggle('light')
+
+    //pegar tag image
+    
+
+    //substituir a imagem
+    //se tiver light mode, adicionar a imagem
+    //se tiver sem lght mode, manter a imagem
+
+}
